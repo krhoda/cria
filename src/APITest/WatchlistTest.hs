928f999c20bacef7078e61fd532be071692dad2b
K <kelseydrhoda@gmail.com>
2020-01-18 22:18:53 -0700

    Watchlist tests now saner

--- src/APITest/WatchlistTest.hs
+++ src/APITest/WatchlistTest.hs
@@ -12,7 +12,7 @@ import Record.Watchlist as Wl
 import Record.Req.WatchlistPost
 import Record.Req.WatchlistSymbolPost
 
-import Data.Text (Text, unpack)
+import Data.Text (Text, pack, unpack)
 
 printWLError :: String -> IO ()
 printWLError x = print (x ++ " Ending Watchlist Test...")
@@ -23,27 +23,105 @@ runWatchlistTest cli = do
   testGetWatchlists cli
   putStrLn "Making New Watchlist\n"
   eitherWL <- makeTestWatchlist cli
-  checkTestWatchlist eitherWL
+  maybeWL <- checkTestWatchlist eitherWL
+  case maybeWL of
+    Nothing -> putStrLn "checkTestWatchlist Failed."
+
+    Just wl -> do
+      success <- checkToggleRoutes cli wl
+
+      if not success
+        then putStrLn "checkToggleRoutes Failed."
+
+        else do
+          delRes <- runReq cli $ (signReq cli deleteWatchlist) (unpack (watchlist_id wl))
+
+          case delRes of
+            Left err -> printWLError $ "deleteWatchList Failed: " ++ show err
+
+            Right (_) -> do
+              putStrLn "Checking for deleted watchlists."
+              wRes <- signAndRun cli getWatchlists
+
+              case wRes of
+                Left err -> printWLError $ "Error: " ++ show err
+
+                Right wlList -> do
+                  shouldBeFalse <- return $ checkForWLID wlList (unpack (watchlist_id wl))
+                  if shouldBeFalse
+                    then printWLError "Still found watchlist after deletion."
+                    else putStrLn "All Watchlist tests successful."
+
+checkForWLID :: [Watchlist] -> String -> Bool
+checkForWLID [] _ = False
+checkForWLID (x : xs) y = if unpack (watchlist_id x) == y
+                            then True
+                            else checkForWLID xs y
 
 makeTestWatchlist :: CriaClient -> IO (Either CriaError Watchlist)
 makeTestWatchlist cli = runReq cli $ (signReq cli createWatchlist) testWL
 
-checkTestWatchlist :: Either CriaError Watchlist -> IO ()
-checkTestWatchlist (Left err) =
-        printWLError $ "Cannot Create Watchlist! Error: " ++ show err
+checkTestWatchlist :: Either CriaError Watchlist -> IO (Maybe Watchlist)
+checkTestWatchlist (Left err) = do
+        printWLError $ "Cannot Create Watchlist! Error: " ++ (show err)
+        return Nothing
 
-checkTestWatchlist (Right wlList) = do
-  print wlList
-  -- putStrLn "Verify it does not have Google..."
-  -- shouldBeFalse <- return checkForSym wl "GOOG"
-  -- if shouldBeFalse
-  --   then printWLError "Found google in test watchlist. Should only find FLWS."
-  --   else do
-  --     putStrLn "Verify it has flowers..."
-  --     shouldBeTrue <- return checkForSym wl "FLWS"
-  --     if (not shouldBeTrue)
-  --       then printWLError "Found google in test watchlist. Should only find FLWS."
-  --       else printWLError "Programmer didn't finish!"
+checkTestWatchlist (Right wl) = do
+  print "!!!"
+  print wl
+  print "!!!"
+  putStrLn "Verify it does not have Google..."
+  shouldBeFalse <- return $ checkForSym (assets wl) "GOOG"
+  if shouldBeFalse
+    then do
+      printWLError "Found google in test watchlist. Should only find FLWS."
+      return Nothing
+
+    else do
+      putStrLn "Verify it has flowers..."
+      shouldBeTrue <- return $ checkForSym (assets wl) "FLWS"
+
+      if not shouldBeTrue
+        then do
+          printWLError ("Did not find initial symbol, FLWS, in test watchlist: " ++ show (assets wl) ++ " ")
+          return Nothing
+
+        else return (Just wl)
+
+checkToggleRoutes :: CriaClient -> Watchlist -> IO (Bool)
+checkToggleRoutes cli wl = do
+  addRes <- toggleWatchSym cli (unpack (watchlist_id wl)) "GOOG" True
+  case addRes of
+    Left err -> do
+      printWLError $ show err
+      return False
+
+    Right wl2 -> do
+      putStrLn "Toggled google!"
+      shouldBeTrue <- return $ checkForSym (assets wl) "GOOG"
+
+      if shouldBeTrue
+        then do
+          printWLError ("Did not find added symbol, GOOG, in test watchlist: " ++ show (assets wl) ++ " ")
+          return shouldBeTrue
+
+        else do
+          putStrLn "Google Found! Now removing..."
+          delRes <- toggleWatchSym cli (unpack (watchlist_id wl2)) "GOOG" False
+          case delRes of
+            Left err -> do
+              printWLError $ show err
+              return False
+            Right wl3 -> do
+                shouldBeFalse <- return $ checkForSym (assets wl3) "GOOG"
+                if not shouldBeFalse
+                  then do
+                    printWLError ("Still found 'deleted' symbol, GOOG, in test watchlist: " ++ show (assets wl) ++ " ")
+                    return False
+                  else do
+                    putStrLn "Successfully deleted Google."
+                    return True
+
 
 googleReq :: WatchlistSymbolPost
 googleReq = WatchlistSymbolPost "GOOG"
@@ -51,69 +129,37 @@ googleReq = WatchlistSymbolPost "GOOG"
 testWL :: WatchlistPost
 testWL = WatchlistPost "flowers" ["FLWS"]
 
--- testWatchlistRoutes :: CriaClient -> Int -> IO Int
--- testWatchlistRoutes cli step = do
---   (nextStep, watchlistID) <- testGetWatchlists cli step
---   case watchlistID of
---     Nothing -> do
---       putStrLn "No ID returned stopping Watchlist tests..."
---         -- TODO: PUT CREATE HERE:
---       return nextStep
---     Just wlID -> do
---       lRes <- runReq cli (signReq cli getWatchlist (unpack wlID))
---       anotherStep <- testPrefix step "Testing Get Watchlist... About to toggle google"
---       testToggleWatchlistGoogle cli lRes
---       finalStep <-  testPrefix step "Toggled Google"
-
---       return finalStep
-
--- testToggleWatchlistGoogle :: CriaClient -> Either CriaError Watchlist -> IO ()
--- testToggleWatchlistGoogle _ (Left x) = print $ "Error: " ++ show x
--- testToggleWatchlistGoogle cli (Right wl) = do
---   toggleWatchingGoogle cli (unpack (watchlist_id wl)) (isWatchingGoogle (assets wl))
-
---         -- finalStep <- toggleWatchingGoogle cli nextStep (isWatchingGoogle asts)
---         -- return finalStep
-
--- isWatchingGoogle :: Maybe [Asset] -> Bool
--- isWatchingGoogle Nothing = False
--- isWatchingGoogle (Just []) = False
--- isWatchingGoogle (Just (x : xs)) = case Ast.symbol x of
---   "GOOG"  -> True
---   _ -> isWatchingGoogle (Just xs)
+checkForSym :: Maybe [Asset] -> String -> Bool
+checkForSym Nothing _ = False
+checkForSym (Just []) _ = False
+checkForSym (Just (x : xs)) y = if Ast.symbol x == (pack y)
+  then True
+  else checkForSym (Just xs) y
 
 
--- toggleWatchingGoogle :: CriaClient -> String -> Bool -> IO ()
--- toggleWatchingGoogle cli wlID True = do
---   res <- runReq cli $ signReq cli deleteSymbolWatchlist wlID "GOOG"
---   handleSimpleRes res print
+toggleWatchSym
+        :: CriaClient
+        -> String
+        -> String
+        -> Bool
+        -> IO (Either CriaError Watchlist)
 
--- toggleWatchingGoogle cli wlID False = do
---   res <- runReq cli $ signReq cli addSymbolWatchlist wlID googleReq
---   handleSimpleRes res print
+toggleWatchSym cli wlID sym True = do
+  res <- runReq cli $ signReq cli deleteSymbolWatchlist wlID sym
+  return res
+
+toggleWatchSym cli wlID sym False = do
+  res <- runReq cli $ signReq cli addSymbolWatchlist wlID (WatchlistSymbolPost (pack sym))
+  return res
 
 testGetWatchlists :: CriaClient -> IO ()
 testGetWatchlists cli = do
   wRes <- signAndRun cli getWatchlists
-  -- nextStep <- testPrefix step "Testing List all Watchlists"
   case wRes of
     Left err -> do
-      putStrLn $ "Error: " ++ show err
+      printWLError $ "Error: " ++ show err
 
     Right wl -> do
       let x:xs = wl
       print x
       print xs
-      -- return (nextStep, Just (Wl.watchlist_id $ head wl))
-
--- handleSimpleRes :: (Show a) => Either a b -> (b -> IO ()) -> IO ()
--- handleSimpleRes x f = do
---   case x of
---     Left y -> putStrLn $ "Error: " ++ show y
---     Right z ->  f z
-
--- handleTestRes :: (Show a) => Either a b -> (a -> IO c) -> (b -> IO c) -> IO c
--- handleTestRes x f g = do
---   case x of
---     Left y -> f y
---     Right z ->  g z
